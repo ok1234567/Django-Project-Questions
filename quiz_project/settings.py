699f00ceae7a98dfb1d0b1cd5973409daf0deeeb
joaosilva9 <joaosilva9@ua.pt>
2024-09-13 22:45:56 +0100

    Revert "admin interface"
    This reverts commit d322dba366c83e2decf56163ca3fb1082639eb86.

--- quiz_project/settings.py
+++ quiz_project/settings.py
@@ -119,6 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",  # Ensure your static files are collected from here
-]
