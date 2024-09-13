177bf476be3273d9d4763006741d2b6150030aeb
joaosilva9 <joaosilva9@ua.pt>
2024-09-13 23:03:37 +0100

    some fixes

--- quiz/urls.py
+++ quiz/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('', start_quiz, name='start_quiz'),
     path('question/<int:question_number>/', question_view, name='question'),
     path('results/', results_view, name='results'),
-    path('admin/questions/', question_list, name='admin_question_list'),
-    path('admin/questions/add/', question_add, name='admin_question_add'),
-    path('admin/questions/edit/<int:pk>/', question_edit, name='admin_question_edit'),
-    path('admin/questions/delete/<int:pk>/', question_delete, name='admin_question_delete'),
+    path('admin/quiz/questions/', question_list, name='admin_question_list'),
+    path('admin/quiz/questions/add/', question_add, name='admin_question_add'),
+    path('admin/quiz/questions/edit/<int:pk>/', question_edit, name='admin_question_edit'),
+    path('admin/quiz/questions/delete/<int:pk>/', question_delete, name='admin_question_delete'),
 ]
