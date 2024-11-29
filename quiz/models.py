6753b765ccd0ce23e60e458304bdf900a03275e3
joaosilva9 <joaosilva9@ua.pt>
2024-11-29 17:56:58 +0000

    update questions

--- quiz/models.py
+++ quiz/models.py
@@ -9,6 +9,7 @@ class Question(models.Model):
     option_d = models.CharField(max_length=255, blank=True, null=True)
     option_e = models.CharField(max_length=255, blank=True, null=True)
     correct_option = models.CharField(max_length=1)  # Assume valores 'A', 'B', 'C', 'D' ou 'E'
+    isNormasAndIntructios = models.BooleanField(default=False)
 
     def __str__(self):
         return self.question_text
